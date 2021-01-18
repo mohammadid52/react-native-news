e3c0a1f8b1112cba2b3e194b809b5933e735b3d8
Mohammad <mohammadidehgamwala5@gmail.com>
2021-01-18 16:17:46 +0530

    Home Screen: Added Title

--- src/api/index.js
+++ src/api/index.js
@@ -1,1 +1,17 @@
+import axios from 'axios';
+
 const IP_URL = 'http://ip-api.com/json';
+
+const TOP_HEADLINES = (country) =>
+  `https://newsapi.org/v2/top-headlines?country=${country}&apiKey=29ec909f68cf4ceeb5a300eddef2cdb0`;
+
+export const getAllNewsByCountry = async (country = 'us') => {
+  try {
+    const news = await axios.get(TOP_HEADLINES(country));
+    if (news.status === 'ok') {
+      return news.data;
+    }
+  } catch (error) {
+    console.error('error @getAllNewsByCountry:', error);
+  }
+};
--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,26 +1,76 @@
 /* eslint-disable react-native/no-inline-styles */
-import React from 'react';
-import { Image, StyleSheet, Text, View } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import {
+  Image,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+  ScrollView,
+  ActivityIndicator,
+} from 'react-native';
+import moment from 'moment';
+import AntDesign from 'react-native-vector-icons/AntDesign';
 
 import { useUser } from '../context';
+import { getAllNewsByCountry } from '../api';
 import AvatarList from '../constants/avatars';
 import { find } from 'lodash';
 
 const Home = () => {
-  const { profileImage } = useUser();
-  console.log(profileImage);
-
   const imagePath = find(AvatarList, { id: 1 }).img;
+  const [news, setNews] = useState(null);
+
+  const renderHeader = () => (
+    <View style={styles.header}>
+      <View style={{ flexDirection: 'row', alignItems: 'center' }}>
+        <Image
+          source={imagePath}
+          style={{
+            height: 45,
+            width: 45,
+            resizeMode: 'contain',
+          }}
+        />
+        <Text style={styles.todayDate}>{moment().format('ll')}</Text>
+      </View>
+      <TouchableOpacity>
+        <AntDesign name="search1" size={20} color={'rgba(0,0,0,0.3)'} />
+      </TouchableOpacity>
+    </View>
+  );
+
+  const renderTitle = () => (
+    <View style={styles.titleContainer}>
+      <Text style={styles.title}>Breaking News</Text>
+    </View>
+  );
+
+  useEffect(() => {
+    fetch(
+      'https://newsapi.org/v2/top-headlines?country=us&apiKey=29ec909f68cf4ceeb5a300eddef2cdb0',
+    )
+      .then((response) => response.json())
+      .then((data) => setNews(data));
+  }, []);
+
+  // const renderFirstNews = () => (
+  //   <Vo
+  // )
+
   return (
     <View style={styles.container}>
-      <View style={styles.header}>
-        <View>
-          <Image
-            source={imagePath}
-            style={{ height: 45, width: 45, resizeMode: 'contain' }}
-          />
-        </View>
-      </View>
+      {renderHeader()}
+      <ScrollView showsVerticalScrollIndicator={false}>
+        {renderTitle()}
+        {!news ? (
+          <ActivityIndicator size={'large'} color={'rgb(26,80,139)'} />
+        ) : (
+          <View>
+            <Text>{news.articles[0].author}</Text>
+          </View>
+        )}
+      </ScrollView>
     </View>
   );
 };
@@ -36,5 +86,20 @@ const styles = StyleSheet.create({
     flexDirection: 'row',
     justifyContent: 'space-between',
     alignItems: 'center',
+  },
+  todayDate: {
+    fontFamily: 'Steradian Medium',
+    color: 'rgba(0,0,0,0.3)',
+    fontSize: 14,
+    letterSpacing: 0.7,
+    marginLeft: 20,
+  },
+  titleContainer: {
+    marginVertical: 30,
+  },
+  title: {
+    fontSize: 40,
+    fontFamily: 'Steradian Medium',
+    color: 'rgb(26,80,139)',
   },
 });

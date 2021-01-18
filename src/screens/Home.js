05139acffb6f139607585ae9b13d9712b4855110
Mohammad <mohammadidehgamwala5@gmail.com>
2021-01-18 17:47:04 +0530

    Home Screen: All News Added

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -12,11 +12,13 @@ import {
 } from 'react-native';
 import moment from 'moment';
 import AntDesign from 'react-native-vector-icons/AntDesign';
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
 
 import { useUser } from '../context';
 import { getAllNewsByCountry } from '../api';
 import AvatarList from '../constants/avatars';
 import { find, orderBy } from 'lodash';
+import { FlatList } from 'react-native-gesture-handler';
 
 const { width } = Dimensions.get('screen');
 
@@ -72,22 +74,93 @@ const Home = () => {
           <Text style={styles.firstCard_title}>
             {firstArticle.title.substring(0, 60) + '...'}
           </Text>
+          <View
+            style={{
+              flexDirection: 'row',
+              justifyContent: 'space-between',
+              alignItems: 'center',
+              marginVertical: 10,
+            }}>
+            <Text style={styles.firstCard_author}>{firstArticle.author}</Text>
+            <Text style={styles.firstCard_publishedAt}>
+              {moment(firstArticle.publishedAt).format('ll')}
+            </Text>
+          </View>
         </View>
       </View>
+    );
+  };
+
+  const renderAllNews = ({ item }) => {
+    return (
+      <View style={styles.newsCard}>
+        <Image
+          source={{ uri: item.urlToImage }}
+          style={styles.newsCard_image}
+        />
+        <View
+          style={{
+            justifyContent: 'space-between',
+            padding: 10,
+            flexShrink: 1,
+          }}>
+          <View>
+            <Text style={styles.newsCard__title}>
+              {item.title.substring(0, 60) + '...'}
+            </Text>
+          </View>
+          <View
+            style={{
+              flexDirection: 'row',
+              alignItems: 'center',
+              justifyContent: 'space-between',
+            }}>
+            <View style={{ flexDirection: 'row', alignItems: 'center' }}>
+              <AntDesign name="calendar" size={17} color={'#8d93ab'} />
+              <Text style={styles.newsCard__bottomText}>
+                {moment(item.publishedAt).format('ll')}
+              </Text>
+            </View>
+            <View style={{ flexDirection: 'row', alignItems: 'center' }}>
+              <MaterialCommunityIcons
+                name="clock-time-five-outline"
+                size={17}
+                color={'#8d93ab'}
+              />
+              <Text style={styles.newsCard__bottomText}>10 min read</Text>
+            </View>
+          </View>
+        </View>
+      </View>
+    );
+  };
+
+  const HeaderComponent = () => {
+    return (
+      <>
+        {renderTitle()}
+        {renderFirstNews()}
+      </>
     );
   };
 
   return (
     <View style={styles.container}>
       {renderHeader()}
-      <ScrollView showsVerticalScrollIndicator={false}>
-        {renderTitle()}
-        {!news ? (
-          <ActivityIndicator size={'large'} color={'rgb(26,80,139)'} />
-        ) : (
-          renderFirstNews()
-        )}
-      </ScrollView>
+
+      {!news ? (
+        <ActivityIndicator size={'large'} color={'rgb(26,80,139)'} />
+      ) : (
+        <FlatList
+          style={{ borderRadius: 40 }}
+          showsVerticalScrollIndicator={false}
+          data={news}
+          keyExtractor={(item) => item.url}
+          ListHeaderComponent={HeaderComponent}
+          ListFooterComponent={() => <View style={{ marginBottom: 20 }} />}
+          renderItem={renderAllNews}
+        />
+      )}
     </View>
   );
 };
@@ -103,6 +176,7 @@ const styles = StyleSheet.create({
     flexDirection: 'row',
     justifyContent: 'space-between',
     alignItems: 'center',
+    marginBottom: 15,
   },
   todayDate: {
     fontFamily: 'Steradian Medium',
@@ -122,7 +196,8 @@ const styles = StyleSheet.create({
   firstCard: {
     backgroundColor: '#fff',
     borderRadius: 25,
-    height: 300,
+    height: 320,
+    marginBottom: 40,
   },
   firstCard_image: {
     height: 200,
@@ -133,5 +208,35 @@ const styles = StyleSheet.create({
     fontSize: 20,
     fontFamily: 'Steradian Medium',
     color: '#0f4c75',
+  },
+  firstCard_author: {
+    fontFamily: 'Steradian Medium',
+    color: '#3282b8',
+  },
+  firstCard_publishedAt: {
+    color: '#3282b8',
+    fontFamily: 'Steradian Medium',
+  },
+  newsCard: {
+    marginVertical: 10,
+    flexDirection: 'row',
+    borderRadius: 16,
+  },
+  newsCard_image: {
+    height: 120,
+    width: 120,
+    resizeMode: 'cover',
+    borderRadius: 16,
+  },
+  newsCard__title: {
+    fontFamily: 'Steradian Medium',
+    fontSize: 15,
+  },
+
+  newsCard__bottomText: {
+    fontFamily: 'Steradian Regular',
+    marginLeft: 5,
+    fontSize: 12,
+    color: '#8d93ab',
   },
 });
